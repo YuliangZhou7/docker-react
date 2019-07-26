94ead3cb153926aacdb6d09deea3696211333883
Yuliang Zhou <yuliangzhou7@gmail.com>
2019-07-26 15:36:08 +1000

    Added Dockerfile for creating prod deployment

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  console.log("Hey im running inside a docker container");
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        {/* <p>
+          Edit <code>src/App.js</code> and save to reload. MWHAAHAHAHAHA I AM DIFFERENT
+        </p> */}
+        <p>TESTING SOME NEW CHANGES</p>
         <a
           className="App-link"
           href="https://reactjs.org"
